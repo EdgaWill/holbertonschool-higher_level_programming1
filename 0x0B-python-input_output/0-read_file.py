c2238d281c6b19d1bc581a6112452778da763717
edgar <edgar-william@hotmail.com>
2022-02-02 03:23:21 +0000

    function that reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""read of a file
+
+    Function:
+        File
+"""
+
+
+def read_file(filename=""):
+    """Funtion
+
+       Args:
+           filename
+    """
+    with open(filename, "r", encoding="UTF-8") as runu:
+        print(runu.read(), end="")
