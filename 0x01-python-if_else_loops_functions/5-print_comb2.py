40a7c6ac22f02100262384a59ab1272cf3f4d5fe
edgar <edgar-william@hotmail.com>
2022-02-09 04:28:25 +0000

    program that prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-print_comb2 = 0
-while print_comb2 <= 99:
-    if print_comb2 != 99:
-        print("{:02d}, ".format(print_comb2), end='')
+print_comb = 0
+while print_comb <= 99:
+    if print_comb != 99:
+        print("{:02d}, ".format(print_comb), end='')
     else:
-        print("{:02d}".format(print_comb2))
-    print_comb2 += 1
+        print("{:02d}".format(print_comb))
+    print_comb += 1
