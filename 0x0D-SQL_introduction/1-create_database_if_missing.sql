ab05efa1c4b548570a3385f510bab1bb30cadc15
edgawill <edgar-william@hotmail.com>
2022-03-09 23:04:15 +0000

    CREATE DATABASE

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,2 @@
--- CREATE DATABASE                                                                                                                                                              CREATE DATABASE IF NOT EXITS hbtn_0c_0;
+-- CREATE DATABASE
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
