537573d8ef9b6243f39ffdf463bc09d8c3c47169
edgar <edgar-william@hotmail.com>
2022-03-10 04:46:53 +0000

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- My genres
+SELECT g.name FROM tv_genres g
+INNER JOIN tv_show_genres
+ON g.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY g.name;
