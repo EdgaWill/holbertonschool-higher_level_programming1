8ff1a94bebb18eaa1dc63edd1209c8deaca5c294
edgar <edgar-william@hotmail.com>
2022-03-10 01:27:25 +0000

    My privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- My privileges
-show GRANTS FOR 'user_0d_1'@'localhost';
-show GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
