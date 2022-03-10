b8ebabc540c7a7fdaa32050273f411883ca8c359
edgar <edgar-william@hotmail.com>
2022-03-10 05:00:11 +0000

    No Comedy tonight!

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- No Comedy tonight!
+SELECT a.title FROM tv_shows a
+WHERE a.title NOT IN (
+		SELECT a.title FROM tv_shows a
+			LEFT JOIN tv_show_genres ap
+				ON a.id = ap.show_id
+					LEFT JOIN tv_genres p
+						ON a.id = at.genre_id
+							WHERE a.name = 'Comedy'
+						)
+						ORDER BY p.title;
