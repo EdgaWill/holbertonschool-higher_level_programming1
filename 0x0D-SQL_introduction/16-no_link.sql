e0dc0206dc27bcee0fdb197a1dc7cdde15bbf1a2
edgar <edgar-william@hotmail.com>
2022-03-11 03:19:40 +0000

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,7 @@
 -- Say my name
-SELECT score,name FROM second_table
+SELECT score, name FROM second_table
 WHERE IS NOT NULL
-ORDER BY score DESC
+ORDER BY score DESC;
+
+INSERT INTO second_table (id,name,score)VALUES(5,'Aria',18);
+INSERT INTO second_table (id,name,score)VALUES(5,'Aria',12);
