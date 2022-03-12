bc9531890bc4b2f3114dbedd64041d0f92625c51
edgar <edgar-william@hotmail.com>
2022-03-12 23:42:51 +0000

    My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- My genres
-SELECT g.name FROM tv_genres g
+SELECT a.name FROM tv_genres a
 INNER JOIN tv_show_genres
-ON g.id = tv_show_genres.genre_id
+ON a.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY g.name;
+ORDER BY a.name;
