c9295f71d970e5519d8cf8a36b7c8a8522eb022a
edgar <edgar-william@hotmail.com>
2022-03-12 22:52:55 +0000

    Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 SELECT A.title title, SUM(B.rate) rating FROM tv_shows A
- INNER JOIN tv_show_ratings B
- ON A.id=B.show_id
- GROUP BY A.title
- ORDER BY B.rate DESC
+INNER JOIN tv_show_ratings B
+ON A.id=B.show_id
+GROUP BY A.title
+ORDER BY B.rate DESC
