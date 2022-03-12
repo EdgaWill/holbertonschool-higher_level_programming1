daa5e941a022c762ca5590c222079a34e5afa9e5
edgar <edgar-william@hotmail.com>
2022-03-12 23:02:09 +0000

    Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Rotten tomatoes
 -- select the rating and the program from two tables
-SELECT A.title title, SUM(B.rate) rating FROM tv_shows A
+SELECT A.title, SUM(B.rate) rating FROM tv_shows A
 INNER JOIN tv_show_ratings B
 ON A.id=B.show_id
 GROUP BY A.title
